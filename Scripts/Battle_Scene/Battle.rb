c85279baf786a815c450c8d3fcdf936cf73c92a6
shashibici <shashibici@gmail.com>
2017-02-07 01:32:57 -0500

    Skills now has types.

--- Scripts/Battle_Scene/Battle.rb
+++ Scripts/Battle_Scene/Battle.rb
@@ -38,6 +38,19 @@ module Battle
 		$game_switches[160] = false
 		$Battle_animation_counter_enemy = 0
 		$Battle_animation_counter_player = 0
+		# 执行特殊技
+		callback_skill = $game_party.active.my_skills
+		for priority in callback_skill.keys.sort
+			for key in callback_skill[priority].keys
+				callback_skill[priority][key].special_func
+			end
+		end
+		callback_skill = $game_monstor_battle.my_skills
+		for priority in callback_skill.keys.sort
+			for key in callback_skill[priority].keys
+				callback_skill[priority][key].special_func
+			end
+		end
 		# 执行锁定技
 		callback_skill = $game_party.active.my_skills
 		for priority in callback_skill.keys.sort
